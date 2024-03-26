0bd73aed56ad0ffd12afa7cc6da8932a3ad98fa7
MitiqueO4 <gennadykriss6@gmail.com>
2024-03-26 11:17:47 -0400

    CSS changes to Navbar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -8,13 +8,11 @@ const Navbar = () => {
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="desktopMenu">
-        {/* Correct usage of className and properly closed Link tags */}
         <Link to="home" smooth={true} duration={1000} className="desktopMenuListItem">Home</Link>
-        <Link to="clients" smooth={true} duration={1000} className="desktopMenuListItem">Clients</Link>
         <Link to="about" smooth={true} duration={1000} className="desktopMenuListItem">About</Link>
         <Link to="hotels" smooth={true} duration={1000} className="desktopMenuListItem">Hotels</Link>
       </div>
-      <button className='desktopMenuBookBtn bn36'>Book Now</button>
+      <button className='desktopMenuBtn'>Log in</button>
     </nav>
   )
 }
