3d49879358f0d36c2c78533ff4ddd5c9e3451e61
RishanSubagar <rishans48@gmail.com>
2024-04-03 21:40:41 -0400

    Add link to forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from "./components/NavBar/navbar";
 import LoginSignup from "./components/LoginSignup/loginsignup";
 import Manager from "./components/Manager/manager";
+import HotelChainCreationForm from './components/HotelChainCreationForm/hotelchaincreationform'; 
+import HotelChainRatingForm from './components/HotelChainRatingForm/hotelchainratingform';
+import HotelCreationForm from './components/HotelCreationForm/hotelcreationform'; 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
@@ -14,8 +17,8 @@ function App() {
           <Route path="/login" element={<LoginSignup />} />
           <Route path="/manager" element={<Manager />} />
           <Route path="/hotelchaincreationform" element={<HotelChainCreationForm />} />
-          <Route path="/hotelcreationform" element={<HotelChainCreationForm />} />
-          <Route path="/hotelchainratingform" element={<HotelChainRatingForm />} />
+          <Route path="/hotelcreationform" element={<HotelChainRatingForm />} />
+          <Route path="/hotelchainratingform" element={<HotelCreationForm />} />
         </Routes>
       </div>
     </Router>
